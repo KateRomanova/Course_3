ccd57dce3a273043944a29632d5674d870cbf3d4
KateRomanova <kate.romanova@gmail.com>
2024-02-26 15:23:10 +0300

    tests added

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from src.utils import load_last_five_executed_operations
 import datetime
 
-last_operations = load_last_five_executed_operations
+last_operations = load_last_five_executed_operations()
 
-for operation in last_operations():
+for operation in last_operations:
+    date = operation['date']
 
-
-    def formatted_date():
+    def formatted_date(date):
         """Переводим формат даты исходника в требуемый формат"""
-        date = operation['date']
         dt = datetime.datetime.fromisoformat(date)
         return datetime.datetime.strftime(dt, '%d.%m.%Y')
 
@@ -16,8 +15,7 @@ for operation in last_operations():
     "извлекаем счет, с которого производится перевод, если он есть"
     operation_from = operation.get('from', '')
 
-
-    def mask_card_number():
+    def mask_card_number(operation_from):
         """Маскируем номер карты, чтобы были видны только первые 6 цифр и последние 4 цифры карты"""
         parts = operation_from.split()
 
@@ -30,13 +28,14 @@ for operation in last_operations():
 
         return masked_from
 
-    def mask_account_number():
+
+    operation_to = operation['to']
+    def mask_account_number(operation_to):
         """Маскируем номер счета, чтобы были видны только последние 4 цифры счета"""
-        operation_to = operation['to']
         masked_account_nr = "Счет **" + operation_to[-4:]
         return masked_account_nr
 
-    print(formatted_date(), operation['description'])
-    print(mask_card_number(), mask_account_number())
+    print(formatted_date(date), operation['description'])
+    print(mask_card_number(operation_from), mask_account_number(operation_to))
     print(operation['operationAmount']['amount'], operation['operationAmount']['currency']['name'])
     print()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,21 @@
-from src.main import mask_card_number
+from src.main import *
 
 
 def test_mask_card_number():
-    assert mask_card_number('visa 1234567891234567') == 'visa 1234 56** **** 4567'
+    assert mask_card_number('visa 1234567891234567') == 'visa 1234 56** **** 4567 -> '
+    assert mask_card_number('american express 9876543210987654') == 'american express 9876 54** **** 7654 -> '
+    assert mask_card_number('') == ''
+
+
+def test_formatted_date():
+    assert formatted_date('2019-07-03T18:35:29.512364') == '03.07.2019'
+    assert formatted_date('2018-06-05') == '05.06.2018'
+
+
+def test_mask_account_number():
+    assert mask_account_number('Счет 64686473678894779589') == 'Счет **9589'
+
+
+def test_print():
+    assert print() == None
+
